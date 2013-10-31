31083d34805c7ce1321798cc10846b9850c35dde
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-10-31 14:06:00 +0700

    Initial express commit

---
+++ config.js
@@ -0,0 +1,8 @@
+config = module.exports = {
+  development: {
+    port: 3000
+  },
+  production: {
+    port: 80
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-var parse = require('./parse'),
-    eyes = require('eyes');
+var express = require('express'),
+    exphbs  = require('express3-handlebars'),
+    parse = require('./parse'),
+    // eyes = require('eyes'),
 
-var inspect = require('eyes').inspector({ maxLength: 4096 });
+    app = express(),
+    config = require('./config')[app.get('env')];
 
-parse('Indonesia_projects.xml', function(err, data) {
-  inspect(data);
-})
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home', {title: "Hello"});
+});
+
+app.use(express.static('public/'));
+app.listen(config.port);
+
+console.log('AidGlasses running in ' + app.get('env') + ' mode on port ' + config.port + '.');
+
+// var inspect = require('eyes').inspector({ maxLength: 4096 });
+
+// parse('Indonesia_projects.xml', function(err, data) {
+//   inspect(data);
+// })
 
 // fs.readFile('Indonesia_projects.xml', function(err, data) {
 //   xml2jsParser.parseString(data, function(err, data) {
